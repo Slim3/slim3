eeb27cad4001eb097b164efe9b51e929fed965a4
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-04-11 13:50:19 +0000

    Fixed the file upload problem.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@11 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3-demo/src/demo/action/UploadAction.java
+++ slim3-demo/src/demo/action/UploadAction.java
@@ -5,12 +5,13 @@ import org.apache.struts.upload.FormFile;
 import org.slim3.struts.annotation.Execute;
 import org.slim3.struts.annotation.Required;
 import org.slim3.struts.util.UploadUtil;
-import org.slim3.struts.web.WebLocator;
 
 public class UploadAction {
 
     @Required
     public FormFile formFile;
+
+    public String message;
 
     @Execute(validate = false)
     public ActionForward index() {
@@ -20,9 +21,8 @@ public class UploadAction {
 
     @Execute(input = "index.jsp")
     public ActionForward upload() {
-        String path = WebLocator.getServletContext().getRealPath(
-                "/WEB-INF/work/" + formFile.getFileName());
-        UploadUtil.write(path, formFile);
+        message = formFile.getFileName() + " is uploaded. The size is "
+                + formFile.getFileSize() + " bytes.";
         return new ActionForward("index.jsp");
     }
 }
