3eca3ce13c4caae54abc2da993acdca539560c69
toshihiro.nakamura <toshihiro.nakamura@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-04-18 04:31:10 +0000

    Ghanged to generate simple names for actual type parameters..
    git-svn-id: https://slim3.googlecode.com/svn/trunk@47 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3-gen/src/main/java/org/slim3/gen/generator/JDOModelMetaGenerator.java
+++ slim3-gen/src/main/java/org/slim3/gen/generator/JDOModelMetaGenerator.java
@@ -1,17 +1,18 @@
 package org.slim3.gen.generator;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.PrimitiveType;
-import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementScanner6;
 import javax.lang.model.util.Elements;
-import javax.lang.model.util.SimpleTypeVisitor6;
 import javax.lang.model.util.Types;
 
 import org.slim3.gen.ProductInfo;
@@ -53,10 +54,9 @@ public class JDOModelMetaGenerator extends ElementScanner6<Void, Printer>
             p.println("package %s;", packageElement.getQualifiedName());
             p.println();
         }
-        p.println("import javax.annotation.Generated;");
-        p.println();
-        p.println("import org.slim3.gae.jdo.AttributeMeta;");
-        p.println("import org.slim3.gae.jdo.ModelMeta;");
+        for (String importName : getImportName(e)) {
+            p.println("import %s;", importName);
+        }
         p.println();
         p.println(
                 "@Generated(value = { \"%s\", \"%s\" }, date = \"%tF %<tT\")",
@@ -73,34 +73,39 @@ public class JDOModelMetaGenerator extends ElementScanner6<Void, Printer>
         return DEFAULT_VALUE;
     }
 
+    protected Set<String> getImportName(TypeElement e) {
+        Set<TypeElement> typeElements = new HashSet<TypeElement>();
+        new ElementScanner6<Void, Set<TypeElement>>() {
+            @Override
+            public Void visitVariable(VariableElement e, Set<TypeElement> p) {
+                if (ElementUtil.isAnnotated(e, Annotations.Persistent)) {
+                    ElementUtil.collectTypeElement(e, p);
+                }
+                return DEFAULT_VALUE;
+            }
+        }.scan(e, typeElements);
+        Set<String> result = new TreeSet<String>();
+        for (Iterator<TypeElement> i = typeElements.iterator(); i.hasNext();) {
+            String name = i.next().toString();
+            if (!name.startsWith("java.lang.")) {
+                result.add(name);
+            }
+        }
+        result.add("javax.annotation.Generated");
+        result.add("org.slim3.gae.jdo.AttributeMeta");
+        result.add("org.slim3.gae.jdo.ModelMeta");
+        return result;
+    }
+
     @Override
     public Void visitVariable(VariableElement e, Printer p) {
         if (ElementUtil.isAnnotated(e, Annotations.Persistent)) {
             p
                     .println(
                             "    public AttributeMeta<%1$s> %2$s = new AttributeMeta<%1$s>(\"%2$s\");",
-                            getTypeText(e), e.getSimpleName());
+                            ElementUtil.toText(e, types), e.getSimpleName());
             p.println();
         }
         return DEFAULT_VALUE;
-    }
-
-    protected String getTypeText(VariableElement e) {
-        String text = e.asType().accept(new SimpleTypeVisitor6<String, Void>() {
-            @Override
-            protected String defaultAction(TypeMirror e, Void p) {
-                return e.toString();
-            }
-
-            @Override
-            public String visitPrimitive(PrimitiveType t, Void p) {
-                return types.boxedClass(t).asType().toString();
-            }
-        }, null);
-
-        if (text.startsWith("java.lang.")) {
-            return text.substring(10);
-        }
-        return text;
     }
 }
--- slim3-gen/src/main/java/org/slim3/gen/util/ElementUtil.java
+++ slim3-gen/src/main/java/org/slim3/gen/util/ElementUtil.java
@@ -1,5 +1,7 @@
 package org.slim3.gen.util;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
@@ -8,7 +10,16 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.ErrorType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.WildcardType;
 import javax.lang.model.util.ElementKindVisitor6;
+import javax.lang.model.util.SimpleElementVisitor6;
+import javax.lang.model.util.SimpleTypeVisitor6;
+import javax.lang.model.util.Types;
 
 public final class ElementUtil {
 
@@ -20,7 +31,7 @@ public final class ElementUtil {
                         @Override
                         public Boolean visitTypeAsAnnotationType(TypeElement e,
                                 Void p) {
-                            return e.getQualifiedName().toString().equals(
+                            return e.getQualifiedName().contentEquals(
                                     annotation);
                         }
                     }, null);
@@ -40,5 +51,131 @@ public final class ElementUtil {
             }
         }
         return false;
+    }
+
+    public static String toText(VariableElement e, Types types) {
+        StringBuilder buf = new StringBuilder();
+        e.asType().accept(new ToText(types), buf);
+        return buf.toString();
+    }
+
+    protected static class ToText extends
+            SimpleTypeVisitor6<Void, StringBuilder> {
+
+        protected final Types types;
+
+        public ToText(Types types) {
+            this.types = types;
+        }
+
+        @Override
+        public Void visitArray(ArrayType t, StringBuilder p) {
+            t.getComponentType().accept(this, p);
+            p.append("[]");
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitPrimitive(PrimitiveType t, StringBuilder p) {
+            types.boxedClass(t).asType().accept(this, p);
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitWildcard(WildcardType t, StringBuilder p) {
+            p.append("?");
+            TypeMirror extendedBound = t.getExtendsBound();
+            if (extendedBound != null) {
+                p.append(" extends ");
+                extendedBound.accept(this, p);
+            }
+            TypeMirror superBound = t.getSuperBound();
+            if (superBound != null) {
+                p.append(" super ");
+                superBound.accept(this, p);
+            }
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitError(ErrorType t, StringBuilder p) {
+            p.append("Object");
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitDeclared(DeclaredType t, StringBuilder p) {
+            appendName(t, p);
+            List<? extends TypeMirror> typeArgs = t.getTypeArguments();
+            if (typeArgs.size() > 0) {
+                p.append("<");
+                for (TypeMirror arg : typeArgs) {
+                    arg.accept(this, p);
+                    p.append(", ");
+                }
+                p.setLength(p.length() - 2);
+                p.append(">");
+            }
+            return DEFAULT_VALUE;
+        }
+
+        protected void appendName(DeclaredType t, StringBuilder p) {
+            t.asElement().accept(
+                    new SimpleElementVisitor6<Void, StringBuilder>() {
+                        @Override
+                        public Void visitType(TypeElement e, StringBuilder p) {
+                            p.append(e.getSimpleName());
+                            return DEFAULT_VALUE;
+                        }
+                    }, p);
+        }
+    }
+
+    public static void collectTypeElement(VariableElement e,
+            Collection<TypeElement> result) {
+        e.asType().accept(new CollectTypeElement(), result);
+    }
+
+    protected static class CollectTypeElement extends
+            SimpleTypeVisitor6<Void, Collection<TypeElement>> {
+        @Override
+        public Void visitArray(ArrayType t, Collection<TypeElement> p) {
+            t.getComponentType().accept(this, p);
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitWildcard(WildcardType t, Collection<TypeElement> p) {
+            TypeMirror extendedBound = t.getExtendsBound();
+            if (extendedBound != null) {
+                extendedBound.accept(this, p);
+            }
+            TypeMirror superBound = t.getSuperBound();
+            if (superBound != null) {
+                superBound.accept(this, p);
+            }
+            return DEFAULT_VALUE;
+        }
+
+        @Override
+        public Void visitDeclared(DeclaredType t, Collection<TypeElement> p) {
+            t.asElement().accept(
+                    new SimpleElementVisitor6<Void, Collection<TypeElement>>() {
+                        @Override
+                        public Void visitType(TypeElement e,
+                                Collection<TypeElement> p) {
+                            p.add(e);
+                            return DEFAULT_VALUE;
+                        }
+                    }, p);
+
+            List<? extends TypeMirror> typeArgs = t.getTypeArguments();
+            if (typeArgs.size() > 0) {
+                for (TypeMirror arg : t.getTypeArguments()) {
+                    arg.accept(this, p);
+                }
+            }
+            return DEFAULT_VALUE;
+        }
     }
 }
