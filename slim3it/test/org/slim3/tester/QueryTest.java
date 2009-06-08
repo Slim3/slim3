5d30c59b8412ffe409f5ab5fee6e076ccde75e51
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-06-08 05:04:53 +0000

    Committed tentatively.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@233 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3it/test/org/slim3/tester/QueryTest.java
@@ -0,0 +1,18 @@
+package org.slim3.tester;
+
+import slim3.it.model.Blog;
+import slim3.it.model.BlogMeta;
+
+public class QueryTest extends JDOTestCase {
+
+    public void testTypeUnsafe() throws Exception {
+        String jdoql =
+            "select from " + Blog.class.getName() + " order by key asc";
+        pm.newQuery(jdoql).execute();
+    }
+
+    public void testTypeSafe() throws Exception {
+        BlogMeta b = new BlogMeta();
+        from(b).orderBy(b.key.asc()).getResultList();
+    }
+}
--- slim3it/test/org/slim3/tester/SpikeTest.java
+++ slim3it/test/org/slim3/tester/SpikeTest.java
@@ -1,6 +1,5 @@
 package org.slim3.tester;
 
-
 public class SpikeTest extends JDOTestCase {
 
     public void test() throws Exception {
