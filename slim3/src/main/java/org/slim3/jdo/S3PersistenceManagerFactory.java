04ab28d4c55afdce43b800b80eb380a4555caff5
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-08-09 03:20:44 +0000

    Removed dependency of App Engine from S3PersistenceManagerFactory.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@357 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3/src/main/java/org/slim3/jdo/S3PersistenceManagerFactory.java
+++ slim3/src/main/java/org/slim3/jdo/S3PersistenceManagerFactory.java
@@ -22,13 +22,10 @@ import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.jdo.JDOFatalUserException;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
-import org.datanucleus.store.appengine.ConcurrentHashMapHelper;
-import org.datanucleus.store.appengine.Utils;
-import org.datanucleus.store.appengine.jdo.DatastoreJDOPersistenceManagerFactory;
+import org.datanucleus.jdo.JDOPersistenceManagerFactory;
 
 /**
  * The persistence manager factory for Slim3.
@@ -37,8 +34,7 @@ import org.datanucleus.store.appengine.jdo.DatastoreJDOPersistenceManagerFactory
  * @since 3.0
  * 
  */
-public class S3PersistenceManagerFactory extends
-        DatastoreJDOPersistenceManagerFactory {
+public class S3PersistenceManagerFactory extends JDOPersistenceManagerFactory {
 
     /**
      * Keeps track of the number of instances we've allocated per PMF in this
@@ -48,18 +44,6 @@ public class S3PersistenceManagerFactory extends
      */
     protected static final ConcurrentHashMap<String, AtomicInteger> NUM_INSTANCES_PER_PERSISTENCE_UNIT =
         new ConcurrentHashMap<String, AtomicInteger>();
-
-    /**
-     * The error message.
-     */
-    protected static final String DUPLICATE_PMF_ERROR_FORMAT =
-        "Application code attempted to create a PersistenceManagerFactory named %s, but "
-            + "one with this name already exists!  Instances of PersistenceManagerFactory are extremely slow "
-            + "to create and it is usually not necessary to create one with a given name more than once.  "
-            + "Instead, create a singleton and share it throughout your code.  If you really do need "
-            + "to create a duplicate PersistenceManagerFactory (such as for a unittest suite), set the "
-            + DISABLE_DUPLICATE_PMF_EXCEPTION_PROPERTY
-            + " system property to avoid this error.";
 
     private static final long serialVersionUID = 1L;
 
@@ -79,26 +63,20 @@ public class S3PersistenceManagerFactory extends
      * @param overridingProps
      *            The Map of properties to initialize the
      *            PersistenceManagerFactory with.
-     * @return A PersistenceManagerFactoryImpl with options set according to the
+     * @return A PersistenceManagerFactory with options set according to the
      *         given Properties.
-     * @throws JDOFatalUserException
-     *             When the user allocates more than one
-     *             {@link PersistenceManagerFactory} with the same name, unless
-     *             the user has added the
-     *             {@link #DISABLE_DUPLICATE_PMF_EXCEPTION_PROPERTY} system
-     *             property.
      * @see JDOHelper#getPersistenceManagerFactory(Map)
      */
-    @SuppressWarnings( { "unchecked", "finally" })
+    @SuppressWarnings( { "unchecked" })
     public synchronized static PersistenceManagerFactory getPersistenceManagerFactory(
             Map overridingProps) {
         // Extract the properties into a Map allowing for a Properties object
         // being used
-        Map<String, Object> overridingMap;
+        Map overridingMap = null;
         if (overridingProps instanceof Properties) {
             // Make sure we handle default properties too (SUN Properties class
             // oddness)
-            overridingMap = new HashMap<String, Object>();
+            overridingMap = new HashMap();
             for (Enumeration e = ((Properties) overridingProps).propertyNames(); e
                 .hasMoreElements();) {
                 String param = (String) e.nextElement();
@@ -108,74 +86,11 @@ public class S3PersistenceManagerFactory extends
         } else {
             overridingMap = overridingProps;
         }
-
-        if (overridingMap == null) {
-            overridingMap = Utils.newHashMap();
-        }
-        // This is an unfortunate way to do things, but I'm not aware of another
-        // way to provide a default value for a specific persistence property
-        // that already has a default value in the core plugin.xml.
-        // plugin.xml in core assigns a default value of UPPERCASE for this
-        // property, but we want a different default value for the app engine
-        // pluging. If I add this is a persistence property to our own
-        // plugin.xml it doesn't always get honored because the plugin
-        // persistence properties are not always read in the same order (there's
-        // a Hashmap buried in there), and the default value is whichever one
-        // gets read first. So, in order to provide a plugin-specific default
-        // value that conflicts with the default value for another plugin,
-        // we set the property to the default value unless the user has
-        // explicitly provided their own value in their config file.
-        if (!overridingMap.containsKey("datanucleus.identifier.case")) {
-            overridingMap.put("datanucleus.identifier.case", "PreserveCase");
-        }
         // Create the PMF and freeze it (JDO spec $11.7)
-        final S3PersistenceManagerFactory pmf =
+        S3PersistenceManagerFactory pmf =
             new S3PersistenceManagerFactory(overridingMap);
         pmf.freezeConfiguration();
-
-        if (alreadyAllocated(pmf.getName())) {
-            try {
-                pmf.close();
-            } finally {
-                // this exception is more important than any exception that
-                // might be
-                // raised by close()
-                throw new JDOFatalUserException(String.format(
-                    DUPLICATE_PMF_ERROR_FORMAT,
-                    pmf.getName()));
-            }
-        }
         return pmf;
-    }
-
-    /**
-     * Determines if the persistence manager factory specified by the name has
-     * been already allocated.
-     * 
-     * @param name
-     *            the persistence manager factory name
-     * 
-     * @return {@code true} if the user has already allocated a
-     *         {@link PersistenceManagerFactory} with the provided name, {@code
-     *         false} otherwise.
-     */
-    protected static boolean alreadyAllocated(String name) {
-        // Not all PMFs have names (like those created by Spring), and since we
-        // do
-        // our duplicate detection based on name we just have to assume that if
-        // there isn't a name it isn't a duplicate. We have to short-circuit
-        // here
-        // because ConcurrentHashMap throws NPE if you pass it a null key.
-        if (name == null) {
-            return false;
-        }
-        AtomicInteger count =
-            ConcurrentHashMapHelper.getCounter(
-                NUM_INSTANCES_PER_PERSISTENCE_UNIT,
-                name);
-        return count.incrementAndGet() > 1
-            && !System.getProperties().containsKey(
-                DISABLE_DUPLICATE_PMF_EXCEPTION_PROPERTY);
     }
 
     @Override
