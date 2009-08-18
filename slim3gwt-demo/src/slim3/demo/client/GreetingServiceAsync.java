59c4fdba27ee4431da79342fcc158b80ad5ac40d
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-08-18 06:05:34 +0000

    Initial commitment.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@385 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3gwt-demo/src/slim3/demo/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package slim3.demo.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+    void greetServer(String input, AsyncCallback<String> callback);
+}
