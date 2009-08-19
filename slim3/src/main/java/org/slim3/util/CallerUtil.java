9ea1a7f45dded36e543f3465b9ff03434e9fb84e
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-08-19 02:14:10 +0000

    Added CallerUtil.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@394 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3/src/main/java/org/slim3/util/CallerUtil.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.slim3.util;
+
+/**
+ * A utility class to access caller information.
+ * 
+ * @author higa
+ * @since 3.0
+ * 
+ */
+public final class CallerUtil {
+
+    private static MySecurityManager sm = new MySecurityManager();
+
+    /**
+     * Returns the current execution stack.
+     * 
+     * @return the current execution stack
+     */
+    public static Class<?>[] getCallerStack() {
+        return sm.getClassContext();
+    }
+
+    /**
+     * Returns the caller class.
+     * 
+     * @return the caller class
+     */
+    public static Class<?> getCaller() {
+        return sm.getClassContext()[3];
+    }
+
+    /**
+     * Returns the caller class loader.
+     * 
+     * @return the caller class loader
+     */
+    public static ClassLoader getCallerClassLoader() {
+        return sm.getClassContext()[3].getClassLoader();
+    }
+
+    private CallerUtil() {
+    }
+
+    private static class MySecurityManager extends SecurityManager {
+
+        @Override
+        protected Class<?>[] getClassContext() {
+            return super.getClassContext();
+        }
+
+    }
+}
