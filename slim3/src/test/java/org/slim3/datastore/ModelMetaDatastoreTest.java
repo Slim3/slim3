ba24d5e014061ee5738ee7032ddf299a65c5b736
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-10-04 02:02:18 +0000

    Refactored ModelMetaDatastoreTest.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@534 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3/src/test/java/org/slim3/datastore/ModelMetaDatastoreTest.java
+++ slim3/src/test/java/org/slim3/datastore/ModelMetaDatastoreTest.java
@@ -227,84 +227,244 @@ public class ModelMetaDatastoreTest extends DatastoreTestCase {
     /**
      * @throws Exception
      */
-    public void testModelToEntityAndEntityToModel() throws Exception {
+    public void testStringText() throws Exception {
         model.setMyStringText("aaa");
-        model.setMyText(new com.google.appengine.api.datastore.Text("aaa"));
-        model.setMyBytes(new byte[] { 1 });
-        model.setMyBytesBlob(new byte[] { 1 });
-        model.setMySerializable(new MySerializable("aaa"));
-        model.setMySerializableBlob(new MySerializable("aaa"));
-
-        model.setMyShortList(Arrays.asList((short) 1));
-        model.setMyShortSet(new HashSet<Short>(Arrays.asList((short) 1)));
-        model.setMyShortSortedSet(new TreeSet<Short>(Arrays.asList((short) 1)));
-
-        model.setMyIntegerList(Arrays.asList(1));
-        model.setMyIntegerSet(new HashSet<Integer>(Arrays.asList(1)));
-        model.setMyIntegerSortedSet(new TreeSet<Integer>(Arrays.asList(1)));
-
-        model.setMyLongList(Arrays.asList(1L));
-        model.setMyLongSet(new HashSet<Long>(Arrays.asList(1L)));
-        model.setMyLongSortedSet(new TreeSet<Long>(Arrays.asList(1L)));
-
-        model.setMyFloatList(Arrays.asList(1f));
-        model.setMyFloatSet(new HashSet<Float>(Arrays.asList(1f)));
-        model.setMyFloatSortedSet(new TreeSet<Float>(Arrays.asList(1f)));
-
         Entity entity = meta.modelToEntity(model);
         Key key = ds.put(entity);
         Entity entity2 = ds.get(key);
         Hoge model2 = meta.entityToModel(entity2);
-
         assertEquals("aaa", model2.getMyStringText());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testText() throws Exception {
+        model.setMyText(new Text("aaa"));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         assertEquals(new Text("aaa"), model2.getMyText());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testBytes() throws Exception {
+        model.setMyBytes(new byte[] { 1 });
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         byte[] myBytes2 = model2.getMyBytes();
         assertEquals(1, myBytes2.length);
         assertEquals(1, myBytes2[0]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testBytesBlob() throws Exception {
+        model.setMyBytesBlob(new byte[] { 1 });
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         byte[] myBytesBlob2 = model2.getMyBytesBlob();
         assertEquals(1, myBytesBlob2.length);
         assertEquals(1, myBytesBlob2[0]);
-        assertEquals(new MySerializable("aaa"), model2.getMySerializable());
-        assertEquals(new MySerializable("aaa"), model2.getMySerializableBlob());
+    }
 
+    /**
+     * @throws Exception
+     */
+    public void testSerializable() throws Exception {
+        model.setMySerializable(new MySerializable("aaa"));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
+        assertEquals(new MySerializable("aaa"), model2.getMySerializable());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testSerializableBlob() throws Exception {
+        model.setMySerializableBlob(new MySerializable("aaa"));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
+        assertEquals(new MySerializable("aaa"), model2.getMySerializableBlob());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testShortList() throws Exception {
+        model.setMyShortList(Arrays.asList((short) 1));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         List<Short> myShortList2 = model2.getMyShortList();
         assertEquals(1, myShortList2.size());
         assertEquals(Short.valueOf("1"), myShortList2.get(0));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testShortSet() throws Exception {
+        model.setMyShortSet(new HashSet<Short>(Arrays.asList((short) 1)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         Set<Short> myShortSet2 = model2.getMyShortSet();
         assertEquals(1, myShortSet2.size());
         assertEquals(Short.valueOf("1"), myShortSet2.iterator().next());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testShortSortedSet() throws Exception {
+        model.setMyShortSortedSet(new TreeSet<Short>(Arrays.asList((short) 1)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         SortedSet<Short> myShortSortedSet2 = model2.getMyShortSortedSet();
         assertEquals(1, myShortSortedSet2.size());
         assertEquals(Short.valueOf("1"), myShortSortedSet2.iterator().next());
+    }
 
+    /**
+     * @throws Exception
+     */
+    public void testIntegerList() throws Exception {
+        model.setMyIntegerList(Arrays.asList(1));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         List<Integer> myIntegerList2 = model2.getMyIntegerList();
         assertEquals(1, myIntegerList2.size());
         assertEquals(Integer.valueOf("1"), myIntegerList2.get(0));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testIntegerSet() throws Exception {
+        model.setMyIntegerSet(new HashSet<Integer>(Arrays.asList(1)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         Set<Integer> myIntegerSet2 = model2.getMyIntegerSet();
         assertEquals(1, myIntegerSet2.size());
         assertEquals(Integer.valueOf("1"), myIntegerSet2.iterator().next());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testIntegerSortedSet() throws Exception {
+        model.setMyIntegerSortedSet(new TreeSet<Integer>(Arrays.asList(1)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         SortedSet<Integer> myIntegerSortedSet2 = model2.getMyIntegerSortedSet();
         assertEquals(1, myIntegerSortedSet2.size());
         assertEquals(Integer.valueOf("1"), myIntegerSortedSet2
             .iterator()
             .next());
+    }
 
+    /**
+     * @throws Exception
+     */
+    public void testLongList() throws Exception {
+        model.setMyLongList(Arrays.asList(1L));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         List<Long> myLongList2 = model2.getMyLongList();
         assertEquals(1, myLongList2.size());
         assertEquals(Long.valueOf("1"), myLongList2.get(0));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testLongSet() throws Exception {
+        model.setMyLongSet(new HashSet<Long>(Arrays.asList(1L)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         Set<Long> myLongSet2 = model2.getMyLongSet();
         assertEquals(1, myLongSet2.size());
         assertEquals(Long.valueOf("1"), myLongSet2.iterator().next());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testLongSortedSet() throws Exception {
+        model.setMyLongSortedSet(new TreeSet<Long>(Arrays.asList(1L)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         SortedSet<Long> myLongSortedSet2 = model2.getMyLongSortedSet();
         assertEquals(1, myLongSortedSet2.size());
         assertEquals(Long.valueOf("1"), myLongSortedSet2.iterator().next());
+    }
 
+    /**
+     * @throws Exception
+     */
+    public void testFloatList() throws Exception {
+        model.setMyFloatList(Arrays.asList(1f));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         List<Float> myFloatList2 = model2.getMyFloatList();
         assertEquals(1, myFloatList2.size());
         assertEquals(Float.valueOf("1"), myFloatList2.get(0));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testFloatSet() throws Exception {
+        model.setMyFloatSet(new HashSet<Float>(Arrays.asList(1f)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         Set<Float> myFloatSet2 = model2.getMyFloatSet();
         assertEquals(1, myFloatSet2.size());
         assertEquals(Float.valueOf("1"), myFloatSet2.iterator().next());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testFloatSortedSet() throws Exception {
+        model.setMyFloatSortedSet(new TreeSet<Float>(Arrays.asList(1f)));
+        Entity entity = meta.modelToEntity(model);
+        Key key = ds.put(entity);
+        Entity entity2 = ds.get(key);
+        Hoge model2 = meta.entityToModel(entity2);
         SortedSet<Float> myFloatSortedSet2 = model2.getMyFloatSortedSet();
         assertEquals(1, myFloatSortedSet2.size());
         assertEquals(Float.valueOf("1"), myFloatSortedSet2.iterator().next());
