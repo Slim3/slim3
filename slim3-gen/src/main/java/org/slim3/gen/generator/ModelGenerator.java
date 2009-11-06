db950f2f71680fa28ae0ffbbafb59d7bcb012880
toshihiro.nakamura <toshihiro.nakamura@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-11-06 04:17:17 +0000

    Supported to generate schemaVersion attribute.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@670 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3-gen/src/main/java/org/slim3/gen/generator/ModelGenerator.java
+++ slim3-gen/src/main/java/org/slim3/gen/generator/ModelGenerator.java
@@ -66,6 +66,8 @@ public class ModelGenerator implements Generator {
         p.println("    @Attribute(version = true)");
         p.println("    private Long version;");
         p.println();
+        p.println("    private Integer schemaVersion;");
+        p.println();
         p.println("    /**");
         p.println("     * Returns the key.");
         p.println("     *");
@@ -102,6 +104,25 @@ public class ModelGenerator implements Generator {
         p.println("     */");
         p.println("    public void setVersion(Long version) {");
         p.println("        this.version = version;");
+        p.println("    }");
+        p.println();
+        p.println("    /**");
+        p.println("     * Returns the schema version.");
+        p.println("     *");
+        p.println("     * @return the schema version");
+        p.println("     */");
+        p.println("    public Integer getSchemaVersion() {");
+        p.println("        return schemaVersion;");
+        p.println("    }");
+        p.println();
+        p.println("    /**");
+        p.println("     * Sets the schema version.");
+        p.println("     *");
+        p.println("     * @param schemaVersion");
+        p.println("     *            the schema version");
+        p.println("     */");
+        p.println("    public void setSchemaVersion(Integer schemaVersion) {");
+        p.println("        this.schemaVersion = schemaVersion;");
         p.println("    }");
         p.println("}");
     }
