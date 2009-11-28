cfc3080b0fac48e33a8fe2b5bf997e34b00b1f3d
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-11-28 13:01:08 +0000

    Fixed Key and AppIdNamespace definitions.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@771 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3/src/main/resources/org/slim3/gwt/emul/com/google/appengine/api/datastore/AppIdNamespace.java
+++ slim3/src/main/resources/org/slim3/gwt/emul/com/google/appengine/api/datastore/AppIdNamespace.java
@@ -6,16 +6,7 @@ public class AppIdNamespace implements Serializable, Comparable {
 
     private static final long serialVersionUID = 1L;
     
-    private String appId;
-    
-    private String namespace;
-    
     private AppIdNamespace() {
-    }
-    
-    public AppIdNamespace(String appId, String namespace) {
-        this.appId = appId;
-        this.namespace = namespace;
     }
     
     public int compareTo(Object o) {
--- slim3/src/main/resources/org/slim3/gwt/emul/com/google/appengine/api/datastore/Key.java
+++ slim3/src/main/resources/org/slim3/gwt/emul/com/google/appengine/api/datastore/Key.java
@@ -5,36 +5,14 @@ import java.io.Serializable;
 public class Key implements Serializable, Comparable {
 
     private static final long serialVersionUID = 1L;
-
-    private Key parentKey;
-    
-    private String kind;
     
     private String appId;    
     
     private long id;
-
-    private String name;
     
     private transient AppIdNamespace appIdNamespace;
         
     private Key() {
-    }
-
-    public String getKind() {
-        return kind;
-    }
-    
-    public Key getParent() {
-        return parentKey;
-    }
-    
-    public long getId() {
-        return id;
-    }
-    
-    public String getName() {
-        return name;
     }
     
     public int compareTo(Object o) {
