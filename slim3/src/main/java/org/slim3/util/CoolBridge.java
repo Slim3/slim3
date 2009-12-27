0bb7cc052eabbb1b0ad069d3e6ace68bd5a46a31
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-12-27 07:06:09 +0000

    Added CoolBridge.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@913 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3/src/main/java/org/slim3/util/CoolBridge.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.slim3.util;
+
+/**
+ * A bridge class between a cool object and a hot reloadable object.
+ * 
+ * @author higa
+ * @since 3.0
+ * 
+ */
+public final class CoolBridge {
+
+    /**
+     * Creates a HOT reloadable object.
+     * 
+     * @param <T>
+     *            type
+     * @param interfaceClass
+     *            the interface class
+     * @return a HOT reloable object
+     * @throws NullPointerException
+     *             if the interfaceClass parameter is null
+     * @throws IllegalArgumentException
+     *             if the class is not an interface or if the class is not under
+     *             cool package
+     * @throws WrapRuntimeException
+     *             if the implementation class is not found
+     */
+    public static <T> T create(Class<T> interfaceClass)
+            throws NullPointerException, IllegalArgumentException,
+            WrapRuntimeException {
+        if (interfaceClass == null) {
+            throw new NullPointerException(
+                "The interfaceClass must not be null.");
+        }
+        String interfaceName = interfaceClass.getName();
+        if (!interfaceClass.isInterface()) {
+            throw new IllegalArgumentException("The class("
+                + interfaceName
+                + ") must be interface.");
+        }
+        if (!interfaceName.contains(".cool.")) {
+            throw new IllegalArgumentException("The class("
+                + interfaceName
+                + ") must be under cool package.");
+        }
+        String implName = interfaceName.replace(".cool.", ".") + "Impl";
+        Class<T> clazz = ClassUtil.forName(implName);
+        if (!interfaceClass.isAssignableFrom(clazz)) {
+            throw new IllegalArgumentException("The class("
+                + implName
+                + ") does not implement "
+                + interfaceName
+                + ".");
+        }
+        return ClassUtil.newInstance(clazz);
+    }
+
+    private CoolBridge() {
+    }
+}
---
+++ slim3/src/test/java/org/slim3/util/CoolBridgeTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.slim3.util;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.Serializable;
+
+import org.junit.Test;
+import org.slim3.util.cool.service.Bad2Service;
+import org.slim3.util.cool.service.BadService;
+import org.slim3.util.cool.service.GreetService;
+
+/**
+ * @author higa
+ * 
+ */
+public class CoolBridgeTest {
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void create() throws Exception {
+        GreetService service = CoolBridge.create(GreetService.class);
+        assertThat(service, is(notNullValue()));
+        assertThat(service.greet(), is("Hello"));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void createWhenClassIsNotInterface() throws Exception {
+        CoolBridge.create(getClass());
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void createWhenClassIsNotUnderCoolPackage() throws Exception {
+        CoolBridge.create(Serializable.class);
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test(expected = WrapRuntimeException.class)
+    public void createWhenImplementationIsNotFound() throws Exception {
+        CoolBridge.create(BadService.class);
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void createWhenImplementationDoesNotImplementInterface()
+            throws Exception {
+        CoolBridge.create(Bad2Service.class);
+    }
+}
