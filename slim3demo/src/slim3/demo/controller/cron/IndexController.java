bea85154af7c728b49b657ced497c18f12622fd3
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2009-12-29 08:59:29 +0000

    Added cron job.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@922 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3demo/src/slim3/demo/controller/cron/IndexController.java
@@ -0,0 +1,12 @@
+package slim3.demo.controller.cron;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class IndexController extends Controller {
+
+    @Override
+    public Navigation run() {
+        return null;
+    }
+}
