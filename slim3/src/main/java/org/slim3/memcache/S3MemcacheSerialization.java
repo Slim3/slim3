4fbe104f7a45f2a73fc1fab974969b9d224993db
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2010-03-08 09:12:57 +0000

    Implemented S3MemcacheSerialization.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@1103 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

---
+++ slim3/src/main/java/org/slim3/memcache/S3MemcacheSerialization.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.slim3.memcache;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+
+import com.google.appengine.api.memcache.InvalidValueException;
+import com.google.appengine.api.memcache.MemcacheSerialization.Flag;
+
+/**
+ * A memcache utility class to serialize and deserialize an object.
+ * 
+ * @author higa
+ * @since 3.0
+ * 
+ */
+public final class S3MemcacheSerialization {
+
+    /**
+     * Deserializes the array of bytes.
+     * 
+     * @param value
+     *            the value
+     * @param flags
+     *            the flags
+     * @return an object
+     * @throws ClassNotFoundException
+     *             if {@link ClassNotFoundException} occurred
+     * @throws IOException
+     *             if {@link IOException} occurred
+     */
+    public static Object deserialize(byte value[], int flags)
+            throws ClassNotFoundException, IOException {
+        Flag flagval = Flag.fromInt(flags);
+        switch (flagval) {
+        case BYTES:
+            return value;
+        case BOOLEAN:
+            if (value.length != 1) {
+                throw new InvalidValueException(
+                    "Cannot deserialize Boolean: bad length");
+            }
+            switch (value[0]) {
+            case 49:
+                return Boolean.TRUE;
+            case 48:
+                return Boolean.FALSE;
+            }
+            throw new InvalidValueException(
+                "Cannot deserialize Boolean: bad contents");
+        case BYTE:
+            return Byte.valueOf(new String(value, "US-ASCII"));
+        case SHORT:
+            return Short.valueOf(new String(value, "US-ASCII"));
+        case INTEGER:
+            return Integer.valueOf(new String(value, "US-ASCII"));
+        case LONG:
+            return Long.valueOf(new String(value, "US-ASCII"));
+        case UTF8:
+            return new String(value, "UTF-8");
+        case OBJECT:
+            if (value.length == 0) {
+                return null;
+            }
+            ByteArrayInputStream bais = new ByteArrayInputStream(value);
+            ObjectInputStream ois = new ObjectInputStream(bais) {
+                @Override
+                protected Class<?> resolveClass(ObjectStreamClass desc)
+                        throws IOException, ClassNotFoundException {
+                    String name = desc.getName();
+                    try {
+                        return Class.forName(name, false, Thread
+                            .currentThread()
+                            .getContextClassLoader());
+                    } catch (ClassNotFoundException ex) {
+                        return super.resolveClass(desc);
+                    }
+                }
+            };
+            Object o = null;
+            try {
+                o = ois.readObject();
+            } finally {
+                ois.close();
+            }
+            return o;
+        }
+        return null;
+    }
+
+    private S3MemcacheSerialization() {
+    }
+}
---
+++ slim3/src/test/java/org/slim3/memcache/S3MemcacheSerializationTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.slim3.memcache;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.slim3.tester.AppEngineTestCase;
+
+import com.google.appengine.api.memcache.MemcacheSerialization;
+import com.google.appengine.api.memcache.MemcacheSerialization.ValueAndFlags;
+
+/**
+ * @author higa
+ * 
+ */
+public class S3MemcacheSerializationTest extends AppEngineTestCase {
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void bytes() throws Exception {
+        byte[] value = new byte[] { 1 };
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((byte[]) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void booleanForTrue() throws Exception {
+        Boolean value = true;
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((Boolean) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void booleanForFalse() throws Exception {
+        Boolean value = false;
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((Boolean) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void testByte() throws Exception {
+        Byte value = 1;
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((Byte) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void testShort() throws Exception {
+        Short value = 1;
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((Short) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Test
+    public void testInteger() throws Exception {
+        Integer value = 1;
+        ValueAndFlags valueAndFlags = MemcacheSerialization.serialize(value);
+        assertThat((Integer) S3MemcacheSerialization.deserialize(
+            valueAndFlags.value,
+            valueAndFlags.flags.ordinal()), is(value));
+    }
+}
