e825f0f9db00eee8b2a9ff6b96c7a60bb8035d4e
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2010-04-06 09:55:32 +0000

    Preparation for the version 1.0.1.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@1195 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3/src/test/java/org/slim3/util/IterableUtilTest.java
+++ slim3/src/test/java/org/slim3/util/IterableUtilTest.java
@@ -33,60 +33,56 @@ public class IterableUtilTest {
     /**
      * @throws Exception
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void splitWithMod() throws Exception {
         List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
         List<Iterable<String>> ret = IterableUtil.split(list, 2);
         assertThat(ret.size(), is(4));
-        assertThat((List) ret.get(0), is(Arrays.asList("1", "2")));
-        assertThat((List) ret.get(1), is(Arrays.asList("3", "4")));
-        assertThat((List) ret.get(2), is(Arrays.asList("5", "6")));
-        assertThat((List) ret.get(3), is(Arrays.asList("7")));
+        assertThat((List<String>) ret.get(0), is(Arrays.asList("1", "2")));
+        assertThat((List<String>) ret.get(1), is(Arrays.asList("3", "4")));
+        assertThat((List<String>) ret.get(2), is(Arrays.asList("5", "6")));
+        assertThat((List<String>) ret.get(3), is(Arrays.asList("7")));
     }
 
     /**
      * @throws Exception
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void splitWithModForNoList() throws Exception {
         List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
         LinkedHashSet<String> set = new LinkedHashSet<String>(list);
         List<Iterable<String>> ret = IterableUtil.split(set, 2);
         assertThat(ret.size(), is(4));
-        assertThat((List) ret.get(0), is(Arrays.asList("1", "2")));
-        assertThat((List) ret.get(1), is(Arrays.asList("3", "4")));
-        assertThat((List) ret.get(2), is(Arrays.asList("5", "6")));
-        assertThat((List) ret.get(3), is(Arrays.asList("7")));
+        assertThat((List<String>) ret.get(0), is(Arrays.asList("1", "2")));
+        assertThat((List<String>) ret.get(1), is(Arrays.asList("3", "4")));
+        assertThat((List<String>) ret.get(2), is(Arrays.asList("5", "6")));
+        assertThat((List<String>) ret.get(3), is(Arrays.asList("7")));
     }
 
     /**
      * @throws Exception
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void splitWithoutMod() throws Exception {
         List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6");
         List<Iterable<String>> ret = IterableUtil.split(list, 2);
         assertThat(ret.size(), is(3));
-        assertThat((List) ret.get(0), is(Arrays.asList("1", "2")));
-        assertThat((List) ret.get(1), is(Arrays.asList("3", "4")));
-        assertThat((List) ret.get(2), is(Arrays.asList("5", "6")));
+        assertThat((List<String>) ret.get(0), is(Arrays.asList("1", "2")));
+        assertThat((List<String>) ret.get(1), is(Arrays.asList("3", "4")));
+        assertThat((List<String>) ret.get(2), is(Arrays.asList("5", "6")));
     }
 
     /**
      * @throws Exception
      */
-    @SuppressWarnings("unchecked")
     @Test
     public void splitWithoutModForNoList() throws Exception {
         List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6");
         LinkedHashSet<String> set = new LinkedHashSet<String>(list);
         List<Iterable<String>> ret = IterableUtil.split(set, 2);
         assertThat(ret.size(), is(3));
-        assertThat((List) ret.get(0), is(Arrays.asList("1", "2")));
-        assertThat((List) ret.get(1), is(Arrays.asList("3", "4")));
-        assertThat((List) ret.get(2), is(Arrays.asList("5", "6")));
+        assertThat((List<String>) ret.get(0), is(Arrays.asList("1", "2")));
+        assertThat((List<String>) ret.get(1), is(Arrays.asList("3", "4")));
+        assertThat((List<String>) ret.get(2), is(Arrays.asList("5", "6")));
     }
 }
