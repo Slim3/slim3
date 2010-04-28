3ec3f4f1e4f036ca213e4cebc8e755b70b9cb637
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2010-04-28 11:52:38 +0000

    Delete unused project
    git-svn-id: https://slim3.googlecode.com/svn/trunk@1257 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3gwt-demo/src/slim3/demo/client/service/GreetingServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package slim3.demo.client.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GreetingServiceAsync {
-
-    void greetServer(String name, AsyncCallback<String> callback);
-
-}
