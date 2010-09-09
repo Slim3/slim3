e383ccc5b3b96f3798b2d930aa3028b2e81a6fc1
higayasuo <higayasuo@b3e2f51c-24b7-11de-8e51-c3b0a9a655b4>
2010-09-09 09:25:21 +0000

    Added Sync annotation sample.
    git-svn-id: https://slim3.googlecode.com/svn/trunk@1346 b3e2f51c-24b7-11de-8e51-c3b0a9a655b4

--- slim3demo/src/slim3/demo/model/Employee.java
+++ slim3demo/src/slim3/demo/model/Employee.java
@@ -14,6 +14,7 @@ public class Employee implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Attribute(primaryKey = true)
+    @Sync(true)
     private Key key;
 
     @Attribute(version = true)
